f3261f8e9041cc2656baf4320a01388edfcf60db
Bryan Westerveld <bryan.westerveld@gmail.com>
2023-05-09 13:03:02 +0200

    Change syntax as mult was not recognized

--- cv32e40p/tests/programs/custom/mult-test-noprintf/mult-test-noprintf.c
+++ cv32e40p/tests/programs/custom/mult-test-noprintf/mult-test-noprintf.c
@@ -15,14 +15,12 @@ int main() {
   __asm__ __volatile__ ("nop");
   __asm__ __volatile__ ("nop");
 
-  int a = 10;
-  int b = 20;
-  int result;
-
-  asm volatile ("mult %1, %2\n\t"
-                "mflo %0"
-                : "=r" (result)
-                : "r" (a), "r" (b));
-    
+  __asm__ __volatile__ (
+      "li $s0, 10\n\t"    // Load 10 into $s0
+      "li $s1, 20\n\t"    // Load 20 into $s1
+      "mult $s0, $s1\n\t" // Multiply $s0 by $s1
+      "mflo $t0\n\t"      // Move the result into $t0
+      "nop\n\t"           // Placeholder for any additional instructions
+  );    
   return 0;
 }
